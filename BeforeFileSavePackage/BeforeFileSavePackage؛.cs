2666b0860f5a992835d44db7f5a326e233e4ab74
ayman <a.reda@crossvertise.com>
2021-01-08 22:41:45 +0100

    Add project files.

---
+++ BeforeFileSavePackage/BeforeFileSavePackage؛.cs
@@ -0,0 +1,40 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace BeforeFileSavePackage
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid(PackageGuidString)]
+    public sealed class BeforeFileSavePackage : AsyncPackage
+    {
+        public const string PackageGuidString = "15fcd3e1-3c5a-4cc2-95b6-f68bfcd26974";
+
+        #region Package Members
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            IVsOutputWindow outWindow = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+
+            new RunningDocTableEvents(this).BeforeSave += BeforeSave;
+        }
+
+        private void BeforeSave(object sender, Document document)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (document.IsValidFile() && !document.EndsWithEmptyLine())
+            {
+                document.AddEmptyLineAtEndOfFile();
+            }
+        }
+        #endregion
+    }
+}
---
+++ BeforeFileSavePackage/DocumentExtensions.cs
@@ -0,0 +1,46 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeforeFileSavePackage
+{
+    public static class DocumentExtensions
+    {
+        private static string[] validFileExtensions = new[] { ".cs", ".ts" };
+
+        public static bool IsValidFile(this Document document)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            var file = new FileInfo(document.FullName);
+            
+            if (validFileExtensions.Contains(file.Extension))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool EndsWithEmptyLine(this Document document)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            var allLines = File.ReadAllLines(document.FullName);
+            if (string.IsNullOrEmpty(allLines[allLines.Length - 1]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void AddEmptyLineAtEndOfFile(this Document document)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            ((TextSelection)document.ProjectItem.Document.Selection).Insert(Environment.NewLine);
+        }
+    }
+}
